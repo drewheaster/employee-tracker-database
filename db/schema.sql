b7a4b5be3f7dc735a83ef6a6c621c701b556deba
Drew Easter <drewheaster@gmail.com>
2024-08-26 02:07:18 -0500

    created mostly functioning database. Need to adjust functionality of manager_id's and updating information

---
+++ db/schema.sql
@@ -0,0 +1,67 @@
+DROP DATABASE IF EXISTS company_db;
+CREATE DATABASE company_db;
+
+\c company_db;
+
+CREATE TABLE department (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(30) UNIQUE NOT NULL
+);
+
+CREATE TABLE role (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(30) UNIQUE NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INTEGER NOT NULL,
+    FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
+);
+
+CREATE TABLE employee (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER NOT NULL,
+    manager_id INTEGER,
+    FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL
+);
+
+INSERT INTO department (name)
+VALUES  ('IT'),
+        ('Marketing'),
+        ('E-Commerce'),
+        ('Quality Control'),
+        ('Sales'),
+        ('Security');
+
+INSERT INTO role (title, salary, department_id)
+VALUES  ('Software Developer II', 200000, 1),
+        ('Software Developer I', 120000, 1),
+        ('Design Lead', 100000, 2),
+        ('Graphic Designer', 60000, 2),
+        ('Photographer', 45000, 2),
+        ('Web Designer', 75000, 2),
+        ('Test Engineer II', 80000, 3),
+        ('Test Engineer I', 50000, 3),
+        ('Sales Lead', 90000, 4),
+        ('Sales Rep II', 60000, 4),
+        ('Sales Rep I', 45000, 4),
+        ('Security Guard', 35000, 5);
+
+-- SET @SoftwareDevManager = (SELECT id FROM employee WHERE role_id IN (SELECT id FROM role WHERE title = 'Software Developer II'));
+-- SET @DesignManager = (SELECT id FROM employee WHERE role_id IN (SELECT id FROM role WHERE title = 'Design Lead'));
+-- SET @TestEngineeringManager = (SELECT id FROM employee WHERE role_id IN (SELECT id FROM role WHERE title = 'Test Engineer II'));
+-- SET @SalesManager = (SELECT id FROM employee WHERE role_id IN (SELECT id FROM role WHERE title = 'Sales Lead'));
+
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES  ('Ava', 'Rodriguez', 1),
+        ('Noah', 'Hernandez', 2),
+        ('Isabella', 'Jones', 3),
+        ('Emma', 'Williams', 4),
+        ('Charlotte', 'Brown', 5),
+        ('Noah', 'Smith', 6),
+        ('Mark', 'Ruben', 7),
+        ('Sophia', 'Davis', 8),
+        ('Charlie', 'Lopez', 9),
+        ('Howard', 'White', 10),
+        ('Matthew', 'Lennis', 11),
+        ('Elijah', 'Miller', 12);
---
+++ index.js
@@ -0,0 +1,132 @@
+const inquirer = require('inquirer');
+const { Pool } = require('pg');
+
+const pool = new Pool(
+  {
+    // Enter PostgreSQL username
+    user: 'postgres',
+    // Enter PostgreSQL password
+    password: 'easterdrew',
+    host: 'localhost',
+    database: 'company_db'
+},
+console.log('Connected to the courses_db database!')
+)
+
+pool.connect();
+
+// view all departments, 
+// view all roles, 
+// view all employees, 
+// add a department, 
+// add a role, 
+// add an employee,
+// update an employee role
+
+const CompanyCMSPrompt = () => {
+    inquirer
+        .prompt([
+            {
+                type: 'list',
+                message: 'What would you like to do?',
+                name: 'initialSelection',
+                choices: [
+                    'View all departments',
+                    'View all roles',
+                    'View all employees',
+                    'Add a department',
+                    'Add a role',
+                    'Add an employee',
+                    'Update an employee role',
+                    'Exit'
+                ]
+            }
+        ]).then((answers) => {
+            const choice = answers.initialSelection;
+
+            switch (choice) {
+                case 'View all departments':
+                    pool.query('SELECT * FROM department', (err, data) => {
+                                console.table(data.rows);
+                                CompanyCMSPrompt();
+                    });
+                break;
+                case 'View all roles':
+                    pool.query('SELECT * FROM role', (err, data) => {
+                        console.table(data.rows);
+                        CompanyCMSPrompt();
+                    });
+                break;
+                case 'View all employees':
+                    pool.query('SELECT * FROM employee', (err, data) => {
+                        console.table(data.rows);
+                        CompanyCMSPrompt();
+                    });
+                break;
+                case 'Add a department':
+                    inquirer.prompt([
+                        {
+                            type: 'input',
+                            message: 'Enter the name of department:',
+                            name: 'department'
+                        }
+                    ]).then((answers) => {
+                        console.log(answers.department);
+                        pool.query(`INSERT INTO department (name) VALUES ('${answers.department}')`, () => {
+                            CompanyCMSPrompt();
+                        });
+                    });
+                break;
+                case 'Add a role':
+                    inquirer.prompt([
+                        {
+                            type: 'input',
+                            message: 'Enter the title of the role:',
+                            name: 'title'
+                        },
+                        {
+                            type: 'input',
+                            message: 'Enter the salary of the role:',
+                            name: 'salary'
+                        },
+                        {
+                            type: 'input',
+                            message: 'Enter the department id of the role:',
+                            name: 'dept_id'
+                        }
+                    ]).then((answers) => {
+                        pool.query(`INSERT INTO role (title, salary, department_id) VALUES ('${answers.title}', ${answers.salary}, ${answers.dept_id})`, () => {
+                            CompanyCMSPrompt();
+                        });
+                    });
+                break;
+                case 'Add an employee':
+                    inquirer.prompt([
+                        {
+                            type: 'input',
+                            message: 'Enter first name:',
+                            name: 'firstName'
+                        },
+                        {
+                            type: 'input',
+                            message: 'Enter last name:',
+                            name: 'lastName'
+                        },
+                        {
+                            type: 'input',
+                            message: 'Enter the role id:',
+                            name: 'roleId'
+                        },
+                    ]).then((answers) => {
+                        pool.query(`INSERT INTO employee (first_name, last_name, role_id, manager_id)) VALUES ('${answers.firstName}', '${answers.lastName}', ${answers.roleId})`, () => {
+                            CompanyCMSPrompt();
+                        });
+                    });
+                break;
+                default: 
+                    process.exit();
+            };
+        });
+};
+
+CompanyCMSPrompt();
